e0e14a96d6703cfe7c89cd1e27e9312ba6e8a320
Ahmed Essam <theartful.ae@gmail.com>
2019-10-17 11:49:35 +0200

    Remove stupid recommendations from the linter

--- src/engine/board.h
+++ src/engine/board.h
@@ -1,9 +1,10 @@
-#ifndef _BOARD_H_
-#define _BOARD_H_
+#ifndef SRC_ENGINE_BOARD_H_
+#define SRC_ENGINE_BOARD_H_
+
+#include <stdint.h>
 
 #include <assert.h>
 #include <chrono>
-#include <stdint.h>
 
 #ifndef NDEBUG
 #include <algorithm>
@@ -12,72 +13,73 @@
 
 namespace go
 {
-	namespace engine
+namespace engine
+{
+enum CellBits : unsigned char
+{
+	WHITE_BIT = 0b00000001,
+	BLACK_BIT = 0b00000010,
+	DEAD_BIT = 0b00010000,
+};
+
+enum class Cell : unsigned char
+{
+	EMPTY = 0,
+	WHITE = WHITE_BIT,
+	BLACK = BLACK_BIT,
+	DEAD_WHITE = DEAD_BIT | WHITE,
+	DEAD_BLACK = DEAD_BIT | BLACK,
+};
+
+enum class Player : unsigned char
+{
+	WHITE = WHITE_BIT,
+	BLACK = BLACK_BIT
+};
+
+struct BoardState
+{
+	static constexpr uint32_t MAX_BOARD_SIZE = 19;
+
+	Cell board[MAX_BOARD_SIZE * MAX_BOARD_SIZE];
+	uint32_t available_white_moves;
+	uint32_t available_black_moves;
+
+	Cell& operator()(uint32_t i, uint32_t j)
 	{
-		enum CellBits : unsigned char
-		{
-			WHITE_BIT = 0b00000001,
-			BLACK_BIT = 0b00000010,
-			DEAD_BIT = 0b00010000,
-		};
-
-		enum class Cell : unsigned char
-		{
-			EMPTY = 0,
-			WHITE = WHITE_BIT,
-			BLACK = BLACK_BIT,
-			DEAD_WHITE = DEAD_BIT | WHITE,
-			DEAD_BLACK = DEAD_BIT | BLACK,
-		};
-
-		enum class Player : unsigned char
-		{
-			WHITE = WHITE_BIT,
-			BLACK = BLACK_BIT
-		};
-
-		struct BoardState
-		{
-			static constexpr uint32_t MAX_BOARD_SIZE = 19;
-
-			Cell board[MAX_BOARD_SIZE * MAX_BOARD_SIZE];
-			uint32_t available_white_moves;
-			uint32_t available_black_moves;
-
-			Cell& operator()(uint32_t i, uint32_t j)
-			{
-				assert(i < MAX_BOARD_SIZE);
-				assert(j < MAX_BOARD_SIZE);
-				return board[i * MAX_BOARD_SIZE + j];
-			}
-		};
-
-		struct Action
-		{
-			uint32_t x;
-			uint32_t y;
-			Player p;
-		};
-
-		struct GameState
-		{
-			BoardState board_state;
-			uint32_t board_size;
-			uint32_t number_played_moves;
-			Player player_turn;
-			// score
-			uint32_t white_score;
-			uint32_t black_score;
-			// time information
-			std::chrono::steady_clock::time_point start_time;
-			std::chrono::duration<uint32_t, std::milli> duration;
-		};
-
-		static inline Cell operator|(Cell cell, CellBits bit)
-		{
-			return static_cast<Cell>(static_cast<unsigned char>(cell) | bit);
-		}
+		assert(i < MAX_BOARD_SIZE);
+		assert(j < MAX_BOARD_SIZE);
+		return board[i * MAX_BOARD_SIZE + j];
 	}
+};
+
+struct Action
+{
+	uint32_t x;
+	uint32_t y;
+	Player p;
+};
+
+struct GameState
+{
+	BoardState board_state;
+	uint32_t board_size;
+	uint32_t number_played_moves;
+	Player player_turn;
+	// score
+	uint32_t white_score;
+	uint32_t black_score;
+	// time information
+	std::chrono::steady_clock::time_point start_time;
+	std::chrono::duration<uint32_t, std::milli> duration;
+};
+
+static inline Cell operator|(Cell cell, CellBits bit)
+{
+	return static_cast<Cell>(static_cast<unsigned char>(cell) | bit);
 }
 
-#endif // _BOARD_H_
+} // namespace engine
+} // namespace go
+
+#endif // SRC_ENGINE_BOARD_H_
--- src/engine/interface.h
+++ src/engine/interface.h
@@ -1,19 +1,21 @@
-#ifndef _INTERFACE_H_
-#define _INTERFACE_H_
+#ifndef SRC_ENGINE_INTERFACE_H_
+#define SRC_ENGINE_INTERFACE_H_
 
 #include "board.h"
 
 namespace go
 {
-    namespace engine
-    {
-        // Plays a move, if legal, changing the board state and game state
-        bool make_move(GameState&, const Action&);
-        // Plays a move, if legal, changing the board state
-        bool make_move(BoardState&, const Action&);
-        bool is_valid_move(BoardState&, const Action&);
-        uint32_t calculate_score(BoardState&, Player);
-    }
-}
+namespace engine
+{
 
-#endif // _INTERFACE_H_
+// Plays a move, if legal, changing the board state and game state
+bool make_move(GameState&, const Action&);
+// Plays a move, if legal, changing the board state
+bool make_move(BoardState&, const Action&);
+bool is_valid_move(BoardState&, const Action&);
+uint32_t calculate_score(BoardState&, Player);
+
+} // namespace engine
+} // namespace go
+
+#endif // SRC_ENGINE_INTERFACE_H_
