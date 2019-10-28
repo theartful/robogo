d65f3d16da86d6f852f0ed6ebbb372e4124bd9bd
Ahmed Essam <theartful.ae@gmail.com>
2019-10-28 20:32:21 +0200

    Add BoardState default constructor

--- src/engine/board.h
+++ src/engine/board.h
@@ -1,8 +1,8 @@
 #ifndef SRC_ENGINE_BOARD_H_
 #define SRC_ENGINE_BOARD_H_
 
+#include <array>
 #include <assert.h>
-#include <bitset>
 #include <chrono>
 #include <stdint.h>
 
@@ -27,8 +27,12 @@ struct BoardState
 	static constexpr uint32_t MAX_NUM_CELLS = MAX_BOARD_SIZE * MAX_BOARD_SIZE;
 	static constexpr uint32_t INVALID_INDEX = MAX_NUM_CELLS;
 
-	Cell board[MAX_NUM_CELLS];
+	std::array<Cell, MAX_NUM_CELLS> board;
 	uint32_t ko;
+
+	BoardState() : board{Cell::EMPTY}, ko{INVALID_INDEX}
+	{
+	}
 
 	Cell& operator()(uint32_t i, uint32_t j)
 	{
