74f3e6ac374a7d9c59f5b24494be1949f293a1ef
Ahmed Ibrahim <ahmed.abdelnaby98@eng-st.cu.edu.eg>
2019-10-29 19:02:57 +0200

    Add game state printing

--- src/SimpleGUI/simplegui.h
+++ src/SimpleGUI/simplegui.h
@@ -3,7 +3,6 @@
 
 namespace go{
 
-<<<<<<< HEAD
 namespace simplegui{
 
 struct BoardSimpleGUI
@@ -21,31 +20,6 @@ struct BoardSimpleGUI
     //print board to console, requires clearing console
     void print_board(const BoardState& board);
 
-=======
-namespace go{
-
-namespace simplegui{
-
-struct BoardSimpleGUI
-{
-    
-    static const int BOARD_SIZE = 19;
-    char board[BOARD_SIZE*BOARD_SIZE];
-
-    BoardSimpleGUI();
-
-    // initialize board at start of the game;
-    void initializeBoard();
-
-    // clears console
-    void clearScreen();
-
-    // prints board to console
-    void printBoard();
-
-    // changes board based on action, points are 1-based
-    void makeMove(int x,int y,bool player);
->>>>>>> 8293200... Fix linter errors
 
 };
 
