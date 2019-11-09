4cf504403ccacc991e4fac4fcb115b41c55a3915
yasmeenahmed <yeasmeenahmed@gmail.com>
2019-11-09 17:25:22 +0200

    adding sgf interface

---
+++ src/SGF _GNU library/SGFinterface.c
@@ -0,0 +1,12 @@
+// Action play_node(SGFNode *node)
+// {
+
+//     SGFProperty *prop;
+//     for (prop = node->props; prop; prop = prop->next)
+//     {
+//         switch (prop->name)
+//         {
+//             case SGFAB: // case add black to the board
+//         }
+//     }
+// }
