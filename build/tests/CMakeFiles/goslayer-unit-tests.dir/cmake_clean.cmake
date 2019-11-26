d245899c937af497354a1d776584f9bb7d365a9e
reemashraf <remoash97@gmail.com>
2019-11-26 23:42:15 +0200

    checking ko validity in board :

---
+++ build/tests/CMakeFiles/goslayer-unit-tests.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/goslayer-unit-tests.dir/main.cpp.o"
+  "CMakeFiles/goslayer-unit-tests.dir/moves.cpp.o"
+  "CMakeFiles/goslayer-unit-tests.dir/sample.cpp.o"
+  "../bin/goslayer-unit-tests.pdb"
+  "../bin/goslayer-unit-tests"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/goslayer-unit-tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
