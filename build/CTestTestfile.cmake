628b8ff3d554a2e2936ed705b88020e2cdc31768
reemashraf <remoash97@gmail.com>
2019-12-07 21:16:03 +0200

    deleting build

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/reem/Data/4th years projects/MI - Project/RoboGo
-# Build directory: /media/reem/Data/4th years projects/MI - Project/RoboGo/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tests")
