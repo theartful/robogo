5528a5c6d0a9baab0330ffe5716ca2bf341da201
yasmeenahmed <yeasmeenahmed@gmail.com>
2019-12-07 20:14:10 +0200

    fixed is pass

--- src/SGF _GNU library/SGFinterface.h
+++ src/SGF _GNU library/SGFinterface.h
@@ -4,7 +4,7 @@
 #include <cstring>
 
 void get_sgf_move(SGFProperty* prop, int& x, int& y, bool& isPass);
-void sgf_play_node(SGFNode *node, uint32_t& index, uint32_t player, bool& has_move);
+void sgf_play_node(SGFNode *node, uint32_t& index, uint32_t player, bool& has_move, bool& isPass);
 std::vector<go::engine::Action> load_sgf_tree(SGFNode* head);
 void get_move_x_y(uint32_t index, char* x, char* y);
 void extract_sgf_file(std::vector<go::engine::Action>& moves, const char* file_name);
