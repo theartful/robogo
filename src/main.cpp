94de1c1b28b87d3594901f7210009fa38d522938
abdo-soliman <abdelrahman.soliman97@eng-st.cu.edu.eg>
2019-12-15 20:37:55 +0200

    added main get_parameter

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 // #include "SGF_GNU_library/SGFinterface.h"
+#include "GUI/agent/agent.h"
+#include "GUI/server/Server.h"
 #include "SimpleGUI/simplegui.h"
 #include "agents/mcts_agent.h"
 #include "controller/game.h"
 #include "engine/board.h"
-#include "GUI/server/Server.h"
-#include "GUI/agent/agent.h"
 #include "gtp/gtp.h"
 #include "net/game_manager.h"
 #include <memory>
@@ -18,25 +18,36 @@ using namespace go::gui;
 Server* s = NULL;
 void sigint_handler(int signal)
 {
-    if (s != NULL)
-        delete s;
-    exit(1);
+	if (s != NULL)
+		delete s;
+	exit(1);
 }
 
-int main(int argc, const char * argv[])
+std::string get_parameter(int argc, const char* argv[], std::string long_parameter, std::string short_parameter)
+{
+	for (int i = 0; i < argc-1; i++)
+	{
+		if (strcmp(argv[i], long_parameter.c_str()) == 0 || strcmp(argv[i], short_parameter.c_str()) == 0)
+			return argv[i+1];
+	}
+
+	return "";
+}
+
+int main(int argc, const char* argv[])
 {
 	struct sigaction sigIntHandler;
-    sigIntHandler.sa_handler = sigint_handler;
-    sigemptyset(&sigIntHandler.sa_mask);
-    sigIntHandler.sa_flags = 0;
-    sigaction(SIGINT, &sigIntHandler, NULL);
+	sigIntHandler.sa_handler = sigint_handler;
+	sigemptyset(&sigIntHandler.sa_mask);
+	sigIntHandler.sa_flags = 0;
+	sigaction(SIGINT, &sigIntHandler, NULL);
 
-	if (argc > 1 && strcmp(argv[1], "gtp") == 0)
+	if (get_parameter(argc, argv, "--mode", "-m") == "gtp")
 	{
 		gtp::GTPEngine engine;
 		engine.game_loop();
 	}
-	else if (argc > 1 && strcmp(argv[1], "debug") == 0)
+	else if (get_parameter(argc, argv, "--mode", "-m") == "debug")
 	{
 		Game game;
 		auto agent1 = std::make_shared<BoardSimpleGUI>();
@@ -51,9 +62,9 @@ int main(int argc, const char * argv[])
 	{
 		char mode1 = 'a';
 		char mode2 = 'a';
-		if (argc > 2 && (strcmp(argv[1], "--port") == 0 || strcmp(argv[1], "-p") == 0))
+		std::string port_str = get_parameter(argc, argv, "--port", "-p");
+		if (port_str != "")
 		{
-			std::string port_str = argv[2];
 			uint32_t port = Server::parse_port(port_str);
 			if (port > 0)
 				s = Server::setup(mode1, mode2, port);
@@ -68,10 +79,20 @@ int main(int argc, const char * argv[])
 
 		if (mode1 == 'r' || mode2 == 'r')
 		{
-			std::cout << ">>>>>>>>net game<<<<<<<<" << std::endl;
+			std::string uri = get_parameter(argc, argv, "--uri", "-u");
+			uri = (uri != "") ? uri : "ws://localhost:8080";
+
+			std::cout << "net_game_uri: " << uri << std::endl;
 			s->net_game = true;
-			net::GameManager GM("ws://localhost:8080", s);
-			GM.run();
+			try
+			{
+				net::GameManager GM(uri, s);
+				GM.run();
+			}
+			catch(const std::exception& e)
+			{
+				std::cerr << e.what() << '\n';
+			}
 		}
 		else
 		{
@@ -94,8 +115,9 @@ int main(int argc, const char * argv[])
 				agent2 = std::make_shared<MCTSAgent>();
 			else
 				throw std::invalid_argument("invalid player 2 mode");
-			
-			std::cout << "running local game mode1: " << mode1 << ", mode2: " << mode2 << std::endl;
+
+			std::cout << "running local game mode1: " << mode1
+			          << ", mode2: " << mode2 << std::endl;
 			agent1->set_player_idx(0);
 			agent2->set_player_idx(1);
 			game.register_agent(agent1, 0);
